4cb816f2a559844437609828509e86d35a6bf9fe
HYM <244781212@qq.com>
2019-08-29 15:00:45 +0800

    基础core api完成，需要学会了jquery在回头来看动态的

---
+++ goubi/Controllers/TodoController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using goubi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace goubi.Controllers
+{
+    [Route("api/todo")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            var todoitems= await _context.TodoItems.ToListAsync();
+            if (todoitems.Count()==0)
+            {
+                return NotFound();
+            }
+            return todoitems;
+        }
+
+        /// <summary>
+        /// 获取详情
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(r=>r.Id==id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            var todo = await _context.TodoItems.FirstOrDefaultAsync(r => r.Id == id);
+            if (todo != null)
+            {
+                todo.Name = item.Name;
+                todo.IsComplete = item.IsComplete;
+                _context.Entry(todo).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            return Ok();
+
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public async Task<IActionResult> PutPatchTodoItem(long id, string name)
+        {
+            var todo = await _context.TodoItems.FirstOrDefaultAsync(r => r.Id == id);
+            if (todo != null)
+            {
+                todo.Name =name;
+                _context.Entry(todo).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(r=>r.Id==id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- goubi/Startup.cs
+++ goubi/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using goubi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +28,8 @@ namespace goubi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("TodoList")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +44,13 @@ namespace goubi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<TodoContext>();
+                context.Database.Migrate();
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
