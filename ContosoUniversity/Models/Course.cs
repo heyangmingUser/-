499a9a07fa8e56a5280291e4eb6b523f39665ef6
HYM <244781212@qq.com>
2019-08-31 17:17:48 +0800

    创建模型

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    /// <summary>
+    /// 课程
+    /// </summary>
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    /// <summary>
+    /// 登记
+    /// </summary>
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+
+
+        public int CourseID { get; set; }
+        public Course Course { get; set; }
+
+        public int StudentID { get; set; }
+        public Student Student { get; set; }
+
+
+        public Grade? Grade { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+}
