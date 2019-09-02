59d0d777e1395911974837fdb984fc010f375e4a
HYM <244781212@qq.com>
2019-09-02 10:42:53 +0800

    添加分页功能及其应用

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -21,21 +21,29 @@ namespace ContosoUniversity.Pages.Students
         public string DateSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
-        public IList<Student> Student { get;set; }
+        public PaginatedList<Student> Student { get;set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string searchString, string currentFilter, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
-            CurrentFilter = searchString;
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
+            CurrentFilter = searchString;
             IQueryable<Student> studentIQ = from s in _context.Student select s;
             if (!String.IsNullOrEmpty(searchString))
             {
                 studentIQ = studentIQ.Where(s => s.LastName.Contains(searchString)
                                        || s.FirstMidName.Contains(searchString));
             }
-
 
             switch (sortOrder)
             {
@@ -52,8 +60,8 @@ namespace ContosoUniversity.Pages.Students
                     studentIQ = studentIQ.OrderBy(s => s.LastName);
                     break;
             }
-
-            Student = await studentIQ.AsNoTracking().ToListAsync();
+            int pageSize = 3;
+            Student = await PaginatedList<Student>.CreateAsync(studentIQ.AsNoTracking(),pageIndex??1,pageSize);
         }
     }
 }
