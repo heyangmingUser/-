9ba6e5d57c3fd48c16712fdb97965050e6a4e7cf
HYM <244781212@qq.com>
2019-09-02 18:05:24 +0800

    添加实体模型

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,7 +20,7 @@ namespace ContosoUniversity.Models
         public int StudentID { get; set; }
         public Student Student { get; set; }
 
-
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
     }
 
