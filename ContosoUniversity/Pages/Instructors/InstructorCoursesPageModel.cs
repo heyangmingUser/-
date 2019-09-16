8be17420bf36bfd08477e85579d644962fafbccc
HYM <244781212@qq.com>
2019-09-16 17:30:24 +0800

    修改更正Instructor

---
+++ ContosoUniversity/Pages/Instructors/InstructorCoursesPageModel.cs
@@ -0,0 +1,72 @@
+﻿using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Pages.Instructors
+{
+    public class InstructorCoursesPageModel : PageModel
+    {
+        public List<AssignedCourseData> AssignedCourseDataList;
+
+        public void PopulateAssignedCourseData(SchoolContext context, Instructor instructor)
+        {
+            var allCourses = context.Courses;
+            var instructorCourses = new HashSet<int>(instructor.CourseAssignments.Select(c => c.CourseID));
+            AssignedCourseDataList = new List<AssignedCourseData>();
+            foreach (var course in allCourses)
+            {
+                AssignedCourseDataList.Add(new AssignedCourseData
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+        }
+        public void UpdateInstructorCourses(SchoolContext context, string[] selectedCourses, Instructor instructorToUpdate)
+        {
+            if (selectedCourses == null)
+            {
+                instructorToUpdate.CourseAssignments = new List<CourseAssignment>();
+                return;
+            }
+
+            //已选课程 hashSet去重复
+            var selectedCoursesHS = new HashSet<string>(selectedCourses);
+            //讲师对应的所有课程
+            var instructorCourses = new HashSet<int>(instructorToUpdate.CourseAssignments.Select(c => c.Course.CourseID));
+            foreach (var course in context.Courses)
+            {
+                //课程包含在已选课程里
+                if (selectedCoursesHS.Contains(course.CourseID.ToString()))
+                {
+                    //不包含在讲师对应的课程里 则进行添加操作
+                    if (!instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.CourseAssignments.Add(
+                            new CourseAssignment
+                            {
+                                InstructorID = instructorToUpdate.ID,
+                                CourseID = course.CourseID
+                            });
+                    }
+                }
+                else
+                {
+                    if (instructorCourses.Contains(course.CourseID))
+                    {
+                        CourseAssignment courseToRemove= instructorToUpdate
+                                .CourseAssignments
+                                .SingleOrDefault(i => i.CourseID == course.CourseID);
+                        context.Remove(courseToRemove);
+                    }
+                }
+            }
+
+        }
+    }
+}
