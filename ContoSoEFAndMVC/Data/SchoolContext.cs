f2b17dcf63f22dda17a13175a86931d4537ba300
HYM <244781212@qq.com>
2019-09-17 17:09:24 +0800

    创建模型

---
+++ ContoSoEFAndMVC/Data/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using ContoSoEFAndMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContoSoEFAndMVC.Data
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //fluent api
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+
+   
+}
